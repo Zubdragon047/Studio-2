7f857d5ddd2e02f116efb543ec00c96cad19dfec
Adam Zabenskie <zubs047@gmail.com>
2025-02-12 15:18:57 -0800

    ball anchor

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,24 +5,26 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 20f;
     [SerializeField] private InputManager inputManager;
+    [SerializeField] private Transform ballAnchor;
     private Rigidbody ballRB;
     private bool isBallLaunched = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // grab reference to RigidBody
         ballRB = GetComponent<Rigidbody>();
-
-        // add listener to onSpacePressed event
-        // if triggered calls LaunchBall
         inputManager.onSpacePressed.AddListener(call: LaunchBall);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
     }
 
     public void LaunchBall()
     {
         if (isBallLaunched) return;
         isBallLaunched = true;
+        transform.parent = null;
+        ballRB.isKinematic = false;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
